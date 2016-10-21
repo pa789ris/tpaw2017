29304c93e1a3a65c09b7057e64edeb45f567ca5d
pa789ris <lassi92390@hotmail.fr>
2016-10-21 11:47:06 +0200

    Update formulaire.js

--- tp3/formulaire.js
+++ tp3/formulaire.js
@@ -2,10 +2,10 @@ $('#Nom').on('shown.bs.modal', function () {
   if ($("#Nom").val()=""){
     $('.modal').modal('show')       //mettre focus sur le champ d'erreur
   }
-  else if (($("#Prenom).val()=""){
+  else if (($("#Prenom").val()=""){
     $('.modal').modal('show')
             }         
-  else if (($("#Prenom).val()=""){
+  else if (($("#date").val()=""){
     $('.modal').modal('show')
             }
   else $('.modal').modal('show')//mettre bienvenue $nom et $prenom
