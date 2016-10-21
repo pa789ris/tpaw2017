c78ef08b603ce472c2bb12f5f456901d4b3e6e02
pa789ris <lassi92390@hotmail.fr>
2016-10-21 12:11:22 +0200

    Update formulaire.js

--- tp3/formulaire.js
+++ tp3/formulaire.js
@@ -1,5 +1,5 @@
 $(document).ready(function ()) {
-    $("#validate").click(function(){
+    $("#validate").click(function()){
         if ($("#Nom").val()=""){
     $('.modal').modal('show');       //mettre focus sur le champ d'erreur
   })
