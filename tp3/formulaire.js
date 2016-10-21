fc3689d2c27d32061eccaa10ae5752f608157cb2
pa789ris <lassi92390@hotmail.fr>
2016-10-22 01:10:01 +0200

    Update formulaire.js

--- tp3/formulaire.js
+++ tp3/formulaire.js
@@ -4,5 +4,10 @@ $(document).ready(function () {
              $('.modal-title').html("bienvenue");
             $('.modal').modal('show');       //mettre focus sur le champ d'erreur)
         }
+        else if ($("#Prenom").val().length<2){
+             $('.modal-title').html("bienvenue");
+            $('.modal').modal('show');       //mettre focus sur le champ d'erreur)
+        }
+        
     });
 });
