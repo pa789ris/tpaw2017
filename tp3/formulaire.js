46f6cd7726fd9d4583c5ff1a56d5f491eb478b98
pa789ris <lassi92390@hotmail.fr>
2016-10-24 19:07:31 +0200

    Update formulaire.js

--- tp3/formulaire.js
+++ tp3/formulaire.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
         }
         else if ($("#Prenom").val().length<5) {
              $('.modal-title').html(" Veuiller entrer votre Prenom Mr "+$("#Nom").val());//mettre mr et extraire nom
-            $('.modal').modal('show');       //mettre focus sur le champ d'erreur)
+            $('.modal').modal('show');      //mettre focus sur le champ d'erreur)
         }
          else if ($("#date").val()=="") {
              $('.modal-title').html(" Entrer une date au format  Mr "+$("#Nom").val());//mettre mr et extraire nom
@@ -16,17 +16,17 @@ $(document).ready(function () {
        }
           else if ($("#Email").val()=="") {
              $('.modal-title').html(" Entrer une adresse mail valide  Mr "+$("#Nom").val());//mettre mr et extraire nom
-            $('.modal').modal('show');       //mettre focus sur le champ d'erreur)
+           $('.modal').modal('show');     //mettre focus sur le champ d'erreur)
              //faire teste pour la date
        }
           else if ($("#adr").val()=="") {
              $('.modal-title').html(" Entrer votre mot de passe  Mr "+$("#Nom").val());//mettre mr et extraire nom
-            $('.modal').modal('show');       //mettre focus sur le champ d'erreur)
+           $('.modal').modal('show');       //mettre focus sur le champ d'erreur)
              //faire teste pour la date
        }
          else {
              $('.modal-title').html(" pas bienvenue");
-            $('.modal').modal('show');       //mettre focus sur le champ d'erreur)
+           $('.modal').modal('show');       //mettre focus sur le champ d'erreur)
         }
          $(".modal-body").html('<img src="https://maps.googleapis.com/maps/api/staticmap?center='+$("#adr").val()+'&zoom=13&size=300x300&maptype=roadmap&markers='+$("#adr").val()+'"/>');
     });
