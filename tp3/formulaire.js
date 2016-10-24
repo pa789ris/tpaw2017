e7738d584aac36bccb525cda61dad0fb1d161677
pa789ris <lassi92390@hotmail.fr>
2016-10-24 19:23:32 +0200

    Update formulaire.js

--- tp3/formulaire.js
+++ tp3/formulaire.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
         if ($("#Nom").val().length<5){
              $('.modal-title').html("Veuiller entrer votre nom");
             $('.modal').modal('show');  
+            $(".modal-body").html('<img src="https://maps.googleapis.com/maps/api/staticmap?center='+adr+'&zoom=13&size=300x300&maptype=roadmap&markers='+adr+'"/>');
             
         }
         else if ($("#Prenom").val().length<5) {
