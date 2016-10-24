c1ee002af8e7889d20b21a5ccf60aedaaf9648ca
pa789ris <lassi92390@hotmail.fr>
2016-10-24 20:02:21 +0200

    Update formulaire.js

--- tp3/formulaire.js
+++ tp3/formulaire.js
@@ -26,7 +26,7 @@ $(document).ready(function () {
        }
          else {
              $('.modal-title').html(" pas bienvenue");
-             <img src="../galilee.png" alt="Logo Galilee"/>
+                 $(".modal-body").html('<img src="../galilee.png" alt="Logo Galilee"/>');
            $('.modal').modal('show');       //mettre focus sur le champ d'erreur)
         }
     });
