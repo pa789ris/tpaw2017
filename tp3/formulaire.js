872161e46a95295c28efabb0be17169d6efcfca5
pa789ris <lassi92390@hotmail.fr>
2016-10-24 19:44:18 +0200

    Update formulaire.js

--- tp3/formulaire.js
+++ tp3/formulaire.js
@@ -21,12 +21,13 @@ $(document).ready(function () {
        }
           else if ($("#adr").val()=="") {
              $('.modal-title').html(" Entrer votre mot de passe  Mr "+$("#Nom").val());//mettre mr et extraire nom
+              $(".modal-body").html('<img src="https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284"/>');
            $('.modal').modal('show');       //mettre focus sur le champ d'erreur)
              //faire teste pour la date
        }
          else {
              $('.modal-title').html(" pas bienvenue");
-             $(".modal-body").html('<img src="https://maps.googleapis.com/maps/api/staticmap?center='+$("#adr").val()+'&zoom=13&size=300x300&maptype=roadmap&markers='+$("#adr").val()+'"/>');
+            
            $('.modal').modal('show');       //mettre focus sur le champ d'erreur)
         }
     });
