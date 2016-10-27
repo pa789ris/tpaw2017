3e38754fcdcc870109ef0b49b058fb5a85d91e3d
pa789ris <lassi92390@hotmail.fr>
2016-10-27 14:48:22 +0200

    Update formulaire.js

--- tp3/formulaire.js
+++ tp3/formulaire.js
@@ -7,32 +7,32 @@ $(document).ready(function () {
             
         }
         else if ($("#Prenom").val().length<5) {
-             $('.modal-title').html(" Mr :  "+$("#Nom").val());//mettre mr et extraire nom
+             $('.modal-title').html(" "+$("#Nom").val());//mettre mr et extraire nom
              $(".modal-body").html(" Veuiller entrer votre Prenom");
             $('.modal').modal('show');      //mettre focus sur le champ d'erreur)
         }
          else if ($("#date").val()=="") {
-             $('.modal-title').html(" Mr :  "+$("#Nom").val());//mettre mr et extraire nom
+             $('.modal-title').html(""+$("#Nom").val());//mettre mr et extraire nom
             $(".modal-body").html(" Veuiller entrer votre date de naissance"); 
             $('.modal').modal('show');       //mettre focus sur le champ d'erreur)
              //faire teste pour la date
        }
          
           else if ($("#adr").val()=="") {
-            $('.modal-title').html(" Mr :  "+$("#Nom").val());
+            $('.modal-title').html(""+$("#Nom").val());
            $(".modal-body").html(" Veuiller entrer une adresse valide");
            $('.modal').modal('show');       //mettre focus sur le champ d'erreur)
              //faire teste pour la date
        }
          else if ($("#Email").val()=="") {
-            $('.modal-title').html(" Mr :  "+$("#Nom").val());//mettre mr et extraire nom
+            $('.modal-title').html(" "+$("#Nom").val());//mettre mr et extraire nom
            $(".modal-body").html(" Veuiller entrer votre mail");   
            $('.modal').modal('show');     //mettre focus sur le champ d'erreur)
              //faire teste pour la date
        }
         
          else {
-             $('.modal-title').html(" pas bienvenue");
+             $('.modal-title').html(" Bienvenue   "+$("#Nom").val());
                    $(".modal-body").html('<img src="https://maps.googleapis.com/maps/api/staticmap?center='+$("#adr").val()+'&zoom=13&size=300x300&maptype=roadmap&markers='+$("#adr").val()+'"/>');
            $('.modal').modal('show');       //mettre focus sur le champ d'erreur)
         }
