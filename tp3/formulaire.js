ed5d990a50a55f83f0e448348fde6df08c374ae0
pa789ris <lassi92390@hotmail.fr>
2016-10-27 14:44:48 +0200

    Update formulaire.js

--- tp3/formulaire.js
+++ tp3/formulaire.js
@@ -7,23 +7,25 @@ $(document).ready(function () {
             
         }
         else if ($("#Prenom").val().length<5) {
-             $('.modal-title').html(" Mr:  "+$("#Nom").val());//mettre mr et extraire nom
+             $('.modal-title').html(" Mr :  "+$("#Nom").val());//mettre mr et extraire nom
              $(".modal-body").html(" Veuiller entrer votre Prenom");
             $('.modal').modal('show');      //mettre focus sur le champ d'erreur)
         }
          else if ($("#date").val()=="") {
-             $('.modal-title').html(" Entrer une date au format  Mr "+$("#Nom").val());//mettre mr et extraire nom
+             $('.modal-title').html(" Mr :  "+$("#Nom").val());//mettre mr et extraire nom
+            $(".modal-body").html(" Veuiller entrer votre date de naissance"); 
             $('.modal').modal('show');       //mettre focus sur le champ d'erreur)
              //faire teste pour la date
        }
           else if ($("#Email").val()=="") {
-             $('.modal-title').html(" Entrer une adresse mail valide  Mr "+$("#Nom").val());//mettre mr et extraire nom
+            $('.modal-title').html(" Mr :  "+$("#Nom").val());//mettre mr et extraire nom
+           $(".modal-body").html(" Veuiller entrer votre mail");   
            $('.modal').modal('show');     //mettre focus sur le champ d'erreur)
              //faire teste pour la date
        }
           else if ($("#adr").val()=="") {
-             $('.modal-title').html(" Entrer votre mot de passe  Mr "+$("#Nom").val());
-               $(".modal-body").html('<img src="https://maps.googleapis.com/maps/api/staticmap?center='+$("#adr")+'&zoom=13&size=300x300&maptype=roadmap&markers='+$("#adr")+'"/>');//mettre mr et extraire nom
+            $('.modal-title').html(" Mr :  "+$("#Nom").val());
+           $(".modal-body").html(" Veuiller entrer une adresse valide");
            $('.modal').modal('show');       //mettre focus sur le champ d'erreur)
              //faire teste pour la date
        }
