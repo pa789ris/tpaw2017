c3238bd6d2e2adc2f2472ecece170499d5b3513c
pa789ris <lassi92390@hotmail.fr>
2016-11-06 18:14:00 +0100

    Update formulaire.js

--- tp3/formulaire.js
+++ tp3/formulaire.js
@@ -33,11 +33,18 @@ $(document).ready(function () {
         
          else {
              $('.modal-title').html(" Bienvenue   "+$("#Nom").val());
-                   $(".modal-body").html('<img src="https://maps.googleapis.com/maps/api/staticmap?center='+$("#adr").val()+'&zoom=13&size=300x300&maptype=roadmap&markers='+$("#adr").val()+'"/>');
+                  $('.modal-body').html('<p>vous etes nee le 20/02/1993 et vous habitez : <br/> <a href="https://developers.google.com/maps/documentation/static-maps/"> <img src="https://maps.googleapis.com/maps/api/staticmap?center=' + $("#adr").val() + '&markers=' + $("#adr").val() + '&size=640x400"/> </a> </p>')
            $('.modal').modal('show');       //mettre focus sur le champ d'erreur)
         }
     });
 });
+
+ 
+       
+            
+            
+  
+
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition, showError);
