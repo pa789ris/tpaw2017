ea907e96e934cd38aa69f340cac57b3e81ed6c37
pa789ris <lassi92390@hotmail.fr>
2016-11-07 12:07:52 +0100

    Update meteo.js

--- tp5/meteo.js
+++ tp5/meteo.js
@@ -6,7 +6,7 @@ window.onload=function(){
 }
 
 
-funtion searchCity(_city){
+function searchCity(_city){
    console.log(searchCity, 'Hello from' +_city);
   var request = new XMLHttpRequest()
   request.open('GET','http://api.openweathermap.org/data/2.5/weather?q=’+_city+’&appid=84b1ad7d5c74af6f4a273abe6e580333',true);
@@ -34,7 +34,7 @@ funtion searchCity(_city){
 }else{
 }
   };
-request.onerror=funtion(){
+request.onerror=function(){
 };
   request.send();
 }
@@ -43,6 +43,6 @@ request.onerror=funtion(){
 
 
 
-funtion searchLatLng(_lat, _Ing){
+function searchLatLng(_lat, _Ing){
    console.log(searchLatLng, 'Hello from' +_lat+','+_Ing);
 }
