f18a98bbfe390289712d24c185c7e4185aae557b
pa789ris <lassi92390@hotmail.fr>
2016-11-07 11:44:49 +0100

    Update meteo.js

--- tp5/meteo.js
+++ tp5/meteo.js
@@ -22,7 +22,9 @@ funtion searchCity(_city){
        var cloud = responseJSON. Cloudiness; 
        var wind = responseJSON.wind.speed; 
        
-       document.getElementById('result1').innerHTML icon;
+     
+       
+       document.getElementById('result1').innerHTML =icon;
        document.getElementById('result2').innerHTML =temp;
        document.getElementById('result3').innerHTML =humidity;
        document.getElementById('result4').innerHTML =cloud;
