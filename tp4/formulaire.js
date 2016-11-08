aa103789348a2b14890918a17fe448822786dc1f
pa789ris <lassi92390@hotmail.fr>
2016-11-08 17:34:15 +0100

    Update formulaire.js

--- tp4/formulaire.js
+++ tp4/formulaire.js
@@ -57,7 +57,7 @@ function getLocation() {
 
 function showPosition(position) {
     var latlon = position.coords.latitude + "," + position.coords.longitude;
-    $("#inputAdresse3").val(latlon);
+    $("#adr").val(latlon);
 
     var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false";
     document.getElementById("demo").innerHTML = "<img src='"+img_url+"'>";
