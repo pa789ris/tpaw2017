33a3eb4d1753aa3caa5f3dfbebb438f6e22e259b
pa789ris <lassi92390@hotmail.fr>
2016-12-31 13:02:08 +0100

    Create formulaire2.js

---
+++ tp3/formulaire2.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+$("#validate").click(function{
+  if(($"nom").val().length<5{
+   $('.modal-title').html("entrer nom");
+   $('.modal-body').html("enter champ");
+   }
