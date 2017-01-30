28475a6f40f0a6941a1fbbe2ccca4e6cf99ebdbe
pa789ris <lassi92390@hotmail.fr>
2017-01-30 10:11:03 +0100

    Update app.js

--- tp6/app.js
+++ tp6/app.js
@@ -1,1 +1,3 @@
 var meteoApp= angular.module('meteoApp', ['ngRoute','meteoControllers']);
+/* App Module */
+var meteoApp = angular.module('meteoApp', ['ngRoute', 'meteoControllers', 'ngMessages']);
